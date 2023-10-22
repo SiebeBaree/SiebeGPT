7eb3775ee2f0392bded808625dd9fab297fde86d
Siebe Baree <siebe.baree@outlook.com>
2023-10-22 20:46:27 +0200

    Added styling

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import DashboardNavBar from "@/components/DashboardNavbar";
+import Sidebar from "@/components/Sidebar";
 
-export default function MainLayout({ children }: {
+export default function DashboardLayout({ children }: {
     children: React.ReactNode;
 }) {
     return (
-        <>
-            {children}
-        </>
+        <div className="h-full relative">
+            <div
+                className="hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 z-[80] bg-accent text-accent-foreground w-72">
+                <Sidebar/>
+            </div>
+            <main className="md:pl-72">
+                <DashboardNavBar/>
+                {children}
+            </main>
+        </div>
     );
 }

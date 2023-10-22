5932b3664861b028bc5f23ad5360be188fa0557d
Siebe Baree <siebe.baree@outlook.com>
2023-10-22 15:13:21 +0200

    Initial commit

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function MainLayout({ children }: {
+    children: React.ReactNode;
+}) {
+    return (
+        <>
+            {children}
+        </>
+    );
+}

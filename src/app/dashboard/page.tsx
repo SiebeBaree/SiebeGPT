4f69e4b9bddc66c00db274af0e1d401f6757e363
Siebe Baree <siebe.baree@outlook.com>
2023-10-23 16:43:07 +0200

    Fixed positioning

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -51,8 +51,7 @@ export default function Dashboard() {
             <div className="mb-8 space-y-4">
                 <h2 className="text-2xl md:text-4xl font-bold text-center">Explore the power of AI</h2>
                 <p className="text-muted-foreground font-light text-sm md:text-lg text-center">Chat with the smartest AI
-                    -
-                    Experience the power of AI</p>
+                    - Experience the power of AI</p>
             </div>
             <div className="px-4 md:px-20 lg:px-32 space-y-4">
                 {tools.map(tool => (
